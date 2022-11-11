53138518f02cafa389b42f9e317951cf026693bb
lukemusu16 <56021197+lukemusu16@users.noreply.github.com>
2022-11-11 20:04:39 +0100

    Loading Thumbnails in dlc Store

---
+++ EmojiJunkie/Assets/Scripts/AssetData.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetData
+{
+    public enum CURRENNCY
+    {
+        Default,
+        Emojicoins
+    }
+
+    private int _id;
+    private string _name;
+    private string _DLCType;
+    private string _thumbnailUrl;
+    private float _price;
+    private CURRENNCY _currency;
+
+    public virtual CURRENNCY Currency
+    { get { return _currency; } set { _currency = value; } }
+
+    public virtual float Price
+    { get { return _price; } set { _price = value; } }
+
+    public virtual string ThumbnailUrl
+    { get { return _thumbnailUrl; } set { _thumbnailUrl = value; } }
+
+    public virtual string Name
+    { get { return _name; } set { _name = value; } }
+
+    public virtual string DLCType
+    { get { return _DLCType; } set { _DLCType = value; } }
+
+    public virtual int Id
+    { get { return _id; } set { _id = value; } }
+
+    public override string ToString()
+    {
+        return ($"The asset has the following values: ID:{Id}, NAME:{Name}, URL:{ThumbnailUrl}, PRICE:{Price}, CURRENCY:{Currency.ToString()}");
+    }
+
+    public AssetData(int id, string name, string url, float price, CURRENNCY currency, string dlcType)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.ThumbnailUrl = url;
+        this.Price = price;
+        this.Currency = currency;
+        this.DLCType = dlcType;
+    }
+}
---
+++ EmojiJunkie/Assets/Scripts/FirebaseStorageController.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using Firebase.Extensions;
+using Firebase.Storage;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirebaseStorageController : MonoBehaviour
+{
+
+    private FirebaseStorage _firebaseStorageInstance;
+    [SerializeField] private GameObject RawImagePrefab;
+    private GameObject _thumbnailContainer;
+    private List<GameObject> _rawImageList;
+    private List<AssetData> _assetData;
+    public enum DownloadType
+    {
+        Thumbnail, Manifest
+    }
+
+    //Singleton
+    public static FirebaseStorageController Instance
+    {
+        get;
+        private set;
+    }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        Instance = this;
+        _firebaseStorageInstance = FirebaseStorage.DefaultInstance;
+    }
+
+    private void Start()
+    {
+        _thumbnailContainer = GameObject.Find("Content");
+        _rawImageList = new List<GameObject>();
+        //Download Manifest
+        DownloadFileAsync("gs://emojijunkie-c258a.appspot.com/manifest.xml", DownloadType.Manifest);
+    }
+
+    public void DownloadFileAsync(string url, DownloadType dType)
+    {
+        // Create a storage reference from our storage service
+        StorageReference imageRef =
+            _firebaseStorageInstance.GetReferenceFromUrl(url);
+
+        // Download in memory with a maximum allowed size of 1MB (1 * 1024 * 1024 bytes)
+        const long maxAllowedSize = 1 * 2048 * 2048;
+        imageRef.GetBytesAsync(maxAllowedSize).ContinueWithOnMainThread(task =>
+        {
+            if (task.IsFaulted || task.IsCanceled)
+            {
+                Debug.LogException(task.Exception);
+                // Uh-oh, an error occurred!
+            }
+            else
+            {
+                byte[] fileContents = task.Result;
+                Debug.Log($"{imageRef.Name} finished downloading!");
+                if (dType == DownloadType.Thumbnail)
+                {
+                    //Load Image
+                    StartCoroutine(LoadDLCItem(fileContents));
+                }
+                else if (dType == DownloadType.Manifest)
+                {
+                    //Load the manifest
+                    StartCoroutine(LoadManifest(fileContents));
+                }
+
+            }
+        });
+    }
+
+    IEnumerator LoadManifest(byte[] fileContents)
+    {
+        //Converting from byte array to String UTF8
+        XDocument manifest = XDocument.Parse(System.Text.Encoding.UTF8.GetString(fileContents));
+        _assetData = new List<AssetData>();
+
+        foreach (XElement elem in manifest.Root.Elements())
+        {
+            //Debug.Log(elem.Element("id")?.Value);
+            string idStr = elem.Element("id")?.Value;
+            int id = (idStr != null) ? int.Parse(idStr) : 0;
+            string nameStr = elem.Element("name")?.Value;
+            string urlStr = elem.Element("thumbnail")?.Element("url")?.Value;
+            string priceStr = elem.Element("price")?.Value;
+            string dlcTypeStr = elem.Element("DLCType")?.Value;
+            float price = (priceStr != null) ? float.Parse(priceStr) : 0f;
+            AssetData.CURRENNCY currency = AssetData.CURRENNCY.Emojicoins;
+
+            AssetData newAsset = new AssetData(id, nameStr, urlStr, price, currency, dlcTypeStr);
+            _assetData.Add(newAsset);
+
+            DownloadFileAsync(newAsset.ThumbnailUrl, DownloadType.Thumbnail);
+        }
+
+        yield return null;
+    }
+
+    IEnumerator LoadDLCItem(byte[] fileContents)
+    {
+        // Display the image inside _imagePlaceholder
+        GameObject DLCItem = Instantiate(RawImagePrefab, _thumbnailContainer.transform.position, Quaternion.identity, _thumbnailContainer.transform);
+        DLCItem.name = "DownloadedImage_" + _rawImageList.Count;
+        Texture2D tex = new Texture2D(1, 1);
+        tex.LoadImage(fileContents);
+        DLCItem.GetComponentInChildren<RawImage>().texture = tex;
+        DLCItem.transform.Find("Price").GetComponent<TMPro.TextMeshProUGUI>().text = _assetData[_rawImageList.Count].Price.ToString();
+        DLCItem.transform.Find("DLCType").GetComponent<TMPro.TextMeshProUGUI>().text = _assetData[_rawImageList.Count].DLCType.ToString();
+        _rawImageList.Add(DLCItem);
+        yield return null;
+    }
+}
